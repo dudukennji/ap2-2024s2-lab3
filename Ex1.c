4cf4ac14b470a80fa076de3bf3e929f887ee5d90
dudukennji <eduardo.kenji2005@gmail.com>
2024-08-28 09:02:50 -0300

    Create Ex1.c

---
+++ Ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("a)Vênus está a 67 milhões de milhas do sol. A letra J pronuncia-se Jota.\n");
+    
+    printf("b)O primeiro número é 44\n");
+    
+    printf("c)O segundo número é 'num1 + 20.5' = 64.5\n");
+    
+    return 0;
+}
